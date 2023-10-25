5c1e1ca61c129556e75ad2c0cc9d29084876c8d1
farukwebcode21 <farukahmadmk@yahoo.com>
2023-10-25 10:35:25 +0600

    modify middleware

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -8,6 +8,7 @@ use App\Mail\ResetPasswordMail;
 use App\Models\User;
 use Exception;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Mail;
 
 class UserController extends Controller {
@@ -21,9 +22,12 @@ class UserController extends Controller {
                 'email'     => 'required|email|unique:users|max:255',
                 'mobile'    => 'required|string|unique:users|max:20',
                 'password'  => 'required|string|min:8',
+
             ]);
 
             // Has the password
+            $hashedPassword = Hash::make($validateData['password']);
+
             $user = User::create([
                 'firstName' => $validateData['firstName'],
                 'lastName'  => $validateData['lastName'],
@@ -38,18 +42,6 @@ class UserController extends Controller {
 
     }
 
-    // public function UserLogin(Request $request) {
-    //     $count = User::where('email', '=', $request->input('email'))->where('password', '=', $request->input('password'))->count();
-    //     if ($count == 1) {
-    //         # User Login -> JWT Token Issue
-    //         $data = JWTToken::CreateUserToken($request->input('email'));
-    //         return ResponseHelper::out('user login successfull', $data, 200);
-    //     } else {
-    //         return ResponseHelper::out('failed', $count, 500);
-    //     }
-
-    // }
-
     public function userLogin(Request $request) {
         $credentials = $request->only('email', 'password');
         if (User::where($credentials)->count() === 1) {
@@ -59,16 +51,6 @@ class UserController extends Controller {
             return ResponseHelper::out('Login failed', null, 500);
         }
     }
-
-    // public function userLogin(Request $request) {
-    //     $credentials = $request->only('email', 'password');
-    //     if (Auth::attempt($credentials)) {
-    //         $token = JWTToken::createUserToken($request->input('email'));
-    //         return ResponseHelper::out('User Login successful', $token, 200);
-    //     } else {
-    //         return ResponseHelper::out('Login failed', null, 500);
-    //     }
-    // }
 
     public function sendOtpCode(Request $request) {
         // validate the input
@@ -91,28 +73,6 @@ class UserController extends Controller {
         }
     }
 
-    // public function verifyOtp(Request $request) {
-    //     $credentials = $request->only('email', 'otp');
-
-    //     if (User::where($credentials)->count() === 1) {
-    //         // OTP Update
-    //         User::where('email', '=', $credentials('email'))->update(['otp' => '0']);
-    //         // Password Reset Token Issue
-    //         $token = JWTToken::createUserTokenForResetPassword($request->input('email'));
-    //         return response()->json([
-    //             'status'  => 'success',
-    //             'message' => 'OTP Verify Successfully',
-    //             'token'   => $token,
-    //         ], 200);
-    //     } else {
-    //         return response()->json([
-    //             'status'  => 'failed',
-    //             'message' => 'otp verification failed',
-    //         ], 501);
-    //     }
-
-    // }
-
     public function VerifyOTP(Request $request) {
         $email = $request->input('email');
         $otp = $request->input('otp');
@@ -126,55 +86,6 @@ class UserController extends Controller {
         }
 
     }
-
-    // public function verifyOtp(Request $request) {
-    //     $email = $request->input('email');
-    //     $otp = $request->input('otp');
-
-    //     // Log inputs for debugging
-    //     Log::info("Email: $email, OTP: $otp");
-
-    //     // Trim inputs to remove leading/trailing spaces
-    //     $email = trim($email);
-    //     $otp = trim($otp);
-
-    //     $count = User::where('email', $email)->where('otp', $otp)->count();
-
-    //     // Log count for debugging
-    //     Log::info("Count: $count");
-
-    //     if ($count == 1) {
-    //         // OTP Update
-    //         User::where('email', $email)->update(['otp' => '0']);
-
-    //         // Password Reset Token Issue
-    //         $token = JWTToken::createUserTokenForResetPassword($email);
-
-    //         // Log token for debugging
-    //         Log::info("Token: $token");
-
-    //         return ResponseHelper::out('OTP Verify Successful', $token, 200);
-    //     } else {
-    //         return ResponseHelper::out('failed', null, 501);
-    //     }
-    // }
-
-    // public function resetPassword(Request $request) {
-    //     try {
-    //         $email = $request->header('email');
-    //         $password = $request->input('password');
-    //         User::where('email', '=', $email)->update(['password' => $password]);
-    //         return response()->json([
-    //             'status'  => 'success',
-    //             'message' => 'password reset successfully',
-    //         ], status: 200);
-    //     } catch (Exception $e) {
-    //         return response()->json([
-    //             'status'  => 'failed',
-    //             'message' => 'something wrong',
-    //         ], status: 401);
-    //     }
-    // }
 
     public function resetPassword(Request $request) {
         try {
--- app/Http/Middleware/TokenVerification.php
+++ app/Http/Middleware/TokenVerification.php
@@ -3,6 +3,7 @@
 namespace App\Http\Middleware;
 
 use App\Helper\JWTToken;
+use App\Helper\ResponseHelper;
 use Closure;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -14,16 +15,16 @@ class TokenVerification {
      * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
      */
     public function handle(Request $request, Closure $next): Response {
+        //    Received token in header
         $token = $request->header('token');
+        // Verify the user token
         $result = JWTToken::verifyUserToken($token);
-        if ($result == 'unauthorized') {
-            return response()->json([
-                'status'  => 'failed',
-                'message' => 'unauthorized',
-            ], 401);
-        } else {
-            $request->headers->set('email', $result);
-            return $next($request);
+        // check if the result indicates unauthorized
+        if ($result === 'unauthorized') {
+            // Return a JSON response for unauthorized access
+            return ResponseHelper::out('Unauthorized', null, 401);
         }
+        $request->headers->set('email', $result);
+        return $next($request);
     }
 }
